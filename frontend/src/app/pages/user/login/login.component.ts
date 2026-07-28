bbbbcbf66ed15d454134d058902a926dfb9ff994
Terekhov Ivan <for-spam32@yandex.ru>
2026-07-28 15:17:06 +0400

    done: simple validation for login

--- frontend/src/app/pages/user/login/login.component.ts
+++ frontend/src/app/pages/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    rememberMe: [false],
+  });
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  isFieldInvalid(fieldName: string): boolean {
+    const input = this.loginForm.get(fieldName);
+    return !!(input && input.invalid && (input.dirty || input.touched));
+  }
+
+  login(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+  }
 }
