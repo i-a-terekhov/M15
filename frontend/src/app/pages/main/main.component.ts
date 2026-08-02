ccce8275f94796ffa7973b957fb8308470400a0d
Terekhov Ivan <for-spam32@yandex.ru>
2026-08-02 20:34:48 +0400

    refactor: carousels on main page

--- frontend/src/app/pages/main/main.component.ts
+++ frontend/src/app/pages/main/main.component.ts
@@ -9,6 +9,60 @@ import { OwlOptions } from "ngx-owl-carousel-o";
 })
 export class MainComponent {
 
+  banners = [
+    {
+      image: 'assets/images/banner/Image(1).png',
+      additionalUpperSpace: true,
+      categoryTitle: 'Предложение месяца',
+      title: 'Продвижение в<br>Instagram для вашего<br>бизнеса <span>-15%</span>!',
+      additionalText: '',
+      additionalLowerSpace: false,
+    },
+    {
+      image: 'assets/images/banner/Image(2).png',
+      additionalUpperSpace: false,
+      categoryTitle: 'Акция',
+      title: 'Нужен грамотный<br><span>копирайтер</span>?',
+      additionalText: 'Весь декабрь у нас действует акция<br>на работу копирайтера.',
+      additionalLowerSpace: true,
+    },
+    {
+      image: 'assets/images/banner/Image(3).png',
+      additionalUpperSpace: false,
+      categoryTitle: 'Новость дня',
+      title: '<span>6 место</span> в ТОП-10<br>SMM-агенств Москвы!',
+      additionalText: 'Мы благодарим каждого, кто<br>голосовал за нас!',
+      additionalLowerSpace: true,
+    },
+  ];
+  bannersOwlOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    margin: 26,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: false
+  };
+
   reviews = [
     {
       image: 'assets/images/reviews-cards/review-01.png',
@@ -28,20 +82,20 @@ export class MainComponent {
     {
       image: 'assets/images/reviews-cards/review1.png',
       name: 'Глаша',
-      text: 'Всё супер!',
+      text: 'Прошла курс по веб-дизайну в АйтиШторме и осталась в полном восторге! Преподаватели объясняют всё на пальцах, а тонна практики помогла мне собрать первое крутое портфолио.',
     },
     {
       image: 'assets/images/reviews-cards/review2.png',
       name: 'Василиса',
-      text: 'Всё более супер, чем супер!',
+      text: 'Регулярно смотрю бесплатные вебинары от АйтиШторма. Информации дают больше, чем на некоторых платных интенсивах. Настоящие профи, которые горят своим делом!',
     },
     {
       image: 'assets/images/reviews-cards/review3.png',
       name: 'Иммануил',
-      text: 'Всё настолько супер, что более супер, менее супер, чем этот супер! Молодцы! Ваще ребята!',
+      text: 'АйтиШторм — это не просто школа, а мощное комьюнити единомышленников. Закрытый чат студентов помог мне найти первых заказчиков и крутых друзей по всей стране.',
     },
   ];
-  customOptions: OwlOptions = {
+  reviewsOwlOptions: OwlOptions = {
     loop: true,
     mouseDrag: false,
     touchDrag: false,
@@ -65,5 +119,7 @@ export class MainComponent {
       }
     },
     nav: false
-  }
+  };
+
+
 }
